1dd441ecd159669af89ea5e9e115a6c593f1977f
Jorge <jromo1506mx@gmail.com>
2025-08-11 17:11:53 -0600

    New

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FuzzifierComponent } from './pages/fuzzifier/fuzzifier.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,FuzzifierComponent],
+  imports: [RouterOutlet,FuzzifierComponent,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { FuzzifierComponent } from './pages/fuzzifier/fuzzifier.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"home",component:FuzzifierComponent},
+    {path:"",redirectTo:"home",pathMatch:"full"}
+];
